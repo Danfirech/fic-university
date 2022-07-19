e21b65d870b5b68096358cea325f7d19ac6f99a1
Dan Possehl <danielbpossehl@gmail.com>
2022-07-19 10:57:38 -0400

    adding footer props

--- functions.php
+++ functions.php
@@ -13,6 +13,9 @@ wp_enqueue_style('custom-googlefonts', '//fonts.googleapis.com/css?family=Roboto
 add_action("wp_enqueue_scripts", "university_files");
 
 function university_features() {
+  register_nav_menu('headerMenuLocation',"Header Menu Location");
+    register_nav_menu('footerLocationOne',"Footer Location One");
+     register_nav_menu('footerLocationTwo',"Footer Location Two");
 
   add_theme_support('title-tag');
 
@@ -20,6 +23,7 @@ function university_features() {
 
 
 add_action("after_setup_theme", "university_features");
+
 
 
 
